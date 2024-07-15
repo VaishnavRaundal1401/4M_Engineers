1627891a609a128b1046e40a33b3305b126168de
Vaishnav Raundal <vaishnavraundal2016@gmail.com>
2024-07-15 23:53:46 +0530

    first commit

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Services.scss'
+const Services = () => {
+  return (
+    <div className='services'>
+      Services
+    </div>
+  )
+}
+
+export default Services
